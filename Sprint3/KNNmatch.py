e057c7867de6281fc86f7eddccdbc3dcbb387da8
song geng <827265717@qq.com>
2019-12-07 15:20:23 -0500

    Add files via upload

---
+++ Sprint3/KNNmatch.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+import numpy as np
+import operator
+import math
+
+def file2matrix(filename):
+    #打开文件
+    fr = open(filename)
+    # 读取文件所有内容
+    arrayOLines = fr.readlines()
+    # 得到文件行数
+    numberOfLines = len(arrayOLines)
+    # 返回的NumPy矩阵,解析完成的数据:numberOfLines行,3列
+    returnMat = np.zeros((numberOfLines, 7))
+    # 返回的分类标签向量
+    classLabelVector = []
+    # 行的索引值
+    index = 0
+    h = {
+        'A1':'Robotics Entry Level', 'A2':'Robotics Medium Level', 'A3':'Robotics High Level',
+        'B1':'Data Analytics Entry Level', 'B2':'Data Analytics Medium Level', 'B3':'Data Analytics High Level',
+        'C1':'Cybersecurity Entry Level', 'C2':'Cybersecurity Medium Level', 'C3':'Cybersecurity High Level'
+        }
+    for line in arrayOLines:
+        # s.strip(rm)，当rm空时,默认删除空白符(包括'\n','\r','\t',' ')
+        line = line.strip()
+        # 使用s.split(str="",num=string,cout(str))将字符串根据'\t'分隔符进行切片。
+        listFromLine = line.split('\t')
+        # 将数据前三列提取出来,存放到returnMat的NumPy矩阵中,也就是特征矩阵
+        returnMat[index, :] = listFromLine[0:7]
+        # insert label
+        classLabelVector.append(h[listFromLine[-1]])
+        index += 1
+    return returnMat, classLabelVector
+
+def classify0(inX, dataSet, labels, k):
+    #numpy函数shape[0]返回dataSet的行数
+    dataSetSize = dataSet.shape[0]
+    #在列向量方向上重复inX共1次(横向),行向量方向上重复inX共dataSetSize次(纵向)
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    #二维特征相减后平方
+    sqDiffMat = diffMat**2
+    #sum()所有元素相加,sum(0)列相加,sum(1)行相加
+    sqDistances = sqDiffMat.sum(axis=1)
+    #开方,计算出距离
+    distances = sqDistances**0.5
+    #返回distances中元素从小到大排序后的索引值
+    sortedDistIndices = distances.argsort()
+    #定一个记录类别次数的字典
+    classCount = {}
+    for i in range(k):
+        #取出前k个元素的类别
+        voteIlabel = labels[sortedDistIndices[i]]
+        #dict.get(key,default=None),字典的get()方法,返回指定键的值,如果值不在字典中返回默认值。
+        #计算类别次数
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    #python3中用items()替换python2中的iteritems()
+    #key=operator.itemgetter(1)根据字典的值进行排序
+    #key=operator.itemgetter(0)根据字典的键进行排序
+    #reverse降序排序字典
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    #返回次数最多的类别,即所要分类的类别
+    return sortedClassCount[0][0]
+
+def get_input(filename):
+    #打开文件
+    fr = open(filename)
+    # 读取文件所有内容
+    arrayOLines = fr.readlines()
+    # 得到文件行数
+    numberOfLines = len(arrayOLines)
+    # 返回的NumPy矩阵,解析完成的数据:numberOfLines行,3列
+    returnMat = np.zeros((numberOfLines, 7))
+    index = 0
+    for line in arrayOLines:
+        # s.strip(rm)，当rm空时,默认删除空白符(包括'\n','\r','\t',' ')
+        line = line.strip()
+        # 使用s.split(str="",num=string,cout(str))将字符串根据'\t'分隔符进行切片。
+        listFromLine = line.split('\t')
+        # 将数据前三列提取出来,存放到returnMat的NumPy矩阵中,也就是特征矩阵
+        returnMat[index, :] = listFromLine[0:7]
+    return returnMat[0]
+
+
+print('loading')
+filename = 'sample.txt'
+username = 'user.txt'
+dataSet,labels = file2matrix(filename)
+k = 10
+array_in = get_input(username)
+outputLabel = classify0(array_in,dataSet,labels,k)
+print('User input array is :', array_in, ', output label is :',outputLabel)
